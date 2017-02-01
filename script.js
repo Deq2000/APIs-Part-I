83596dbd61b7e692de10790d2278e909344ff61f
deq2000 <inazu2014@gmail.com>
2017-02-01 23:33:34 +0000

    setIntervals

--- script.js
+++ script.js
@@ -5,15 +5,34 @@ $(document).ready(function() {
         var query = $("#query").val();
         console.log(query);
         $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC"
+            url: "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=100"
         }).done(function(response) {
             console.log(response);
-            for (i=0; i<response.data.length; i++) {
+           
+            var insertImage = function (url) {
+                var image = $('<img>');
+                image.attr('src', url);
+                $("#results").append(image);
+            }; 
+            
+            insertImage(response.data[0].images.fixed_width_downsampled.url);
+            var i=1;
+            setInterval(function(){ 
+                $('#results').empty();
+                insertImage(response.data[i].images.fixed_width_downsampled.url);
+                i++;
+                if(i===response.data.length){
+                    i=0;
+                }
+            }, 1000);
+            
+            /*for (i=0; i<response.data.length; i++) {
                 var image = $('<img>');
                 console.log(image);
                 image[0].src = response.data[i].images.fixed_width_downsampled.url;
                 $("#results").append(image);
             }
+            */
         });
     });
 });
